37bc39383deb1396b4d33c61b069e6d664b84d23
Yaroslav <yarsitdikov@gmail.com>
2023-03-24 17:40:54 +0600

    add response request

--- src/pages/menu/menu.jsx
+++ src/pages/menu/menu.jsx
@@ -1,27 +1,44 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Nav from '../../components/nav/nav';
+import { HOST, menuPoint } from '../../end-points';
 import { setPhilial } from '../../set-philial';
 import './menu.css';
 
-const Menu = ({ menuArr }) => {
-    const currentMenu = menuArr.find(el => el.philial === setPhilial)
+const Menu = () => {
+    const [menu, setMenu] = useState([])
+    let currentMenu = [];
+    let dishes = [];
 
-    const dishes = currentMenu.dishes.map((el, i) => (
-        <div className='block' key={i}>
-            <h1>Блюдо: {el.name}</h1>
-            <h2>Цена: {el.price}</h2>
-        </div>
-    ))
+    useEffect(() => {
+        axios.get(`${HOST}${menuPoint}`)
+        .then(res => setMenu(res.data))
+      }, [])
+
+      menu? currentMenu = menu.find(el => el.philial === setPhilial) : currentMenu = null
+      menu && currentMenu? dishes = currentMenu.dishes : dishes = null
 
     return (
         <div>
             <Nav current='menu' />
             <div className='menu'>
                 <div>
-                    <h1>Название: {currentMenu.name}</h1>
-                    <h2>Описание: {currentMenu.description}</h2>
+                    <h1>Название: {currentMenu? currentMenu.name : ''}</h1>
+                    <h2>Описание: {currentMenu? currentMenu.description : ''}</h2>
                 </div>
-                <div className='container'>{dishes}</div>
+                <div className='container'>
+                    {
+                        dishes? 
+                        dishes.map((el, i) => (
+                            <div key={i} className='block'>
+                                <h1>Блюдо: {el.name}</h1>
+                                <h2>Цена: {el.price}</h2>
+                            </div>
+                        ))
+                        :
+                        ''
+                    }
+                </div>
             </div>
         </div>
     );
