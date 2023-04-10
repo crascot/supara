2e8633b8083c60078df999238bac05198f3b132c
Yaroslav <yarsitdikov@gmail.com>
2023-04-10 13:23:19 +0600

    adaptive pages

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import facebook from '../../assets/ico-fb.svg';
 import instagram from '../../assets/ico-in.svg';
 import youtube from '../../assets/ico-yo.svg';
+import menu from '../../assets/menu.png';
 import './nav.css';
 
 const Nav = ({ current }) => {
@@ -23,6 +24,7 @@ const Nav = ({ current }) => {
             text: 'Войти',
         },
     ]
+    const [isActive, setIsActive] = useState(false)
 
     const switchMap = switchArray.map((el, i) => <a href={`/supara/${el.type}`} className={current === el.type ? 'active' : ''} key={i}>{el.text}</a>)
 
@@ -50,12 +52,24 @@ const Nav = ({ current }) => {
                 </ul>
             </div>
             <div className="logo">
-                <a href="/">
+                <a href="/" className='supara-logo'>
                     <img src="https://supara.kg/chunkurchak/wp-content/uploads/sites/3/2018/01/logo-chunkurchakbw-sm.png" alt="" />
                 </a>
-                <div className='switch'>
-                    {switchMap}
-                </div>
+                {
+                    window.innerWidth >= 500 ?
+                        <div className='switch'>
+                            {switchMap}
+                        </div>
+                        :
+                        <>
+                            <div className='dropdown-button'>
+                                <button onClick={() => setIsActive(!isActive)}><img src={menu} alt='' /></button>
+                            </div>
+                            <div className={`switch dropdown ${isActive && 'show'}`}>
+                                {switchMap}
+                            </div>
+                        </>
+                }
             </div>
         </div>
     )
